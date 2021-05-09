d807f31ebe3f7bb525d641aeb9bb648490df925f
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-05-09 22:26:33 +0300

    Add templates and staticfiles.

--- ecommerce_website/ecommerce_website/settings.py
+++ ecommerce_website/ecommerce_website/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ecommerce_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 STATIC_URL = '/static/'
 
